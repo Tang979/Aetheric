d4bba56d2baca1b820e52bb39707482bad22a316
Doan Van Minh <139115619+DoanVanMinh-2180606677@users.noreply.github.com>
2025-06-01 16:31:16 +0700

    vm_v1

---
+++ Assets/script/EnemyMovement.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class EnamyMovement : MonoBehaviour
+{
+    [Header("References")]
+    private Rigidbody2D rb;
+
+    [Header("Attributes")]
+    [SerializeField] private float moveSpeed = 2f;
+
+    private Transform target;
+    private int partIndex = 0;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        if (LevelManager.main == null)
+        {
+            Debug.LogError("LevelManager.main is NULL!");
+            return;
+        }
+
+        if (LevelManager.main.Path.Length == 0)
+        {
+            Debug.LogError("Path array is empty!");
+            return;
+        }
+
+        target = LevelManager.main.Path[partIndex];
+    }
+
+    void Update()
+    {
+        if (Vector2.Distance(target.position, transform.position) <= 0.1f)
+        {
+            partIndex++;
+            if (partIndex == LevelManager.main.Path.Length)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            else
+            {
+                target = LevelManager.main.Path[partIndex];
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (rb != null && target != null)
+        {
+            Vector2 direction = (target.position - transform.position).normalized;
+            rb.linearVelocity = direction * moveSpeed; // đổi lại nếu chưa dùng linearVelocity
+        }
+    }
+}
