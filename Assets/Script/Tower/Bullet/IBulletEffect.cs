916d451d04f32e89705d576abdd72256491edd68
Tang Nguyen Tuan Khoa <tuankhoa542003@gmail.com>
2025-07-10 13:11:05 +0700

    workshop

--- Assets/Script/Tower/Bullet/IBulletEffect.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public interface IBulletEffect 
-{
-    void ApplyEffect(Transform target);
-}
--- Assets/Script/Tower/ITowerAttack.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public interface ITowerAttack
-{
-    void Init(TowerInstance tower);
-    void Tick(float deltaTime);
-}
